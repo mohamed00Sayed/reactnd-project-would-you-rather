be39a14cade91e7148b8971f499b6bf0d9a803ed
mohamed00Sayed <sayedm22529@gmail.com>
2021-09-16 21:47:12 +0200

    feat: adding avatars to users

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -2,7 +2,7 @@ let users = {
   sarahedo: {
     id: 'sarahedo',
     name: 'Sarah Edo',
-    avatarURL: ,
+    avatarURL: 'https://raw.githubusercontent.com/mohamed00Sayed/reactnd-would-you-rather/master/public/avatars/avatar2.png',
     answers: {
       "8xf0y6ziyjabvozdd253nd": 'optionOne',
       "6ni6ok3ym7mf1p33lnez": 'optionTwo',
@@ -14,7 +14,7 @@ let users = {
   tylermcginnis: {
     id: 'tylermcginnis',
     name: 'Tyler McGinnis',
-    avatarURL: ,
+    avatarURL: 'https://raw.githubusercontent.com/mohamed00Sayed/reactnd-would-you-rather/master/public/avatars/avatar3.png',
     answers: {
       "vthrdm985a262al8qx3do": 'optionOne',
       "xj352vofupe1dqz9emx13r": 'optionTwo',
@@ -24,7 +24,7 @@ let users = {
   johndoe: {
     id: 'johndoe',
     name: 'John Doe',
-    avatarURL: ,
+    avatarURL: 'https://raw.githubusercontent.com/mohamed00Sayed/reactnd-would-you-rather/master/public/avatars/avatar1.png',
     answers: {
       "xj352vofupe1dqz9emx13r": 'optionOne',
       "vthrdm985a262al8qx3do": 'optionTwo',
