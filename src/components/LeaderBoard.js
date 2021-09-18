ab2ebecb43d74dea21aac2d2bccf9dd8edb071ed
mohamed00Sayed <sayedm22529@gmail.com>
2021-09-18 15:43:52 +0200

    feat: adding LeaderBoard component

---
+++ src/components/LeaderBoard.js
@@ -0,0 +1,14 @@
+import { Component } from 'react'
+import { connect } from 'react-redux'
+
+class LeaderBoard extends Component{
+  render(){
+    return(
+      <div>
+        Leader Board
+      </div>
+    )
+  }
+}
+
+export default connect()(LeaderBoard)
