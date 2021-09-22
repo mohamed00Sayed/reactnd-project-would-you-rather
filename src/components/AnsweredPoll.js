52e3455c2b7e1f73daf360cbf416f8c0a50155f3
mohamed00Sayed <sayedm22529@gmail.com>
2021-09-22 19:44:32 +0200

    fix: fix choice marker position

--- src/components/AnsweredPoll.js
+++ src/components/AnsweredPoll.js
@@ -21,7 +21,7 @@ class AnsweredPoll extends Component{
               <div className='option-percent' style={{width: `${optOnePercent}%`}}>{optOnePercent}% </div>
               <p>{optOneVotes} of {optOneVotes + optTwoVotes} votes</p>
               {
-                vote === 'first'? (<div className='user-choice' style={{'width':'30px', 'height':'30px','fill':'#017a9b'}}><svg viewBox="0 0 33 32"><path d="M17 7a1 1 0 01.117 1.993L17 9h-7v14h14v-7l.007-.117a1 1 0 01.876-.876L25 15a1 1 0 01.993.883L26 16v7a2 2 0 01-2 2H10a2 2 0 01-2-2V9a2 2 0 012-2h7zm8.555.168a1 1 0 01.277 1.387l-8 10a1 1 0 01-1.54.152l-3-3a1 1 0 011.415-1.414l2.138 2.137 7.323-8.985a1 1 0 011.387-.277z"></path></svg></div>):(<p></p>)
+                vote === 'first'? (<div className='user-choice'><svg viewBox="0 0 33 32"><path d="M17 7a1 1 0 01.117 1.993L17 9h-7v14h14v-7l.007-.117a1 1 0 01.876-.876L25 15a1 1 0 01.993.883L26 16v7a2 2 0 01-2 2H10a2 2 0 01-2-2V9a2 2 0 012-2h7zm8.555.168a1 1 0 01.277 1.387l-8 10a1 1 0 01-1.54.152l-3-3a1 1 0 011.415-1.414l2.138 2.137 7.323-8.985a1 1 0 011.387-.277z"></path></svg></div>):(<p></p>)
               }
             </div>
             <div className='poll-second-option'>
@@ -29,7 +29,7 @@ class AnsweredPoll extends Component{
               <div className='option-percent' style={{width: `${optTwoPercent}%`}}>{optTwoPercent}% </div>
               <p>{optTwoVotes} of {optOneVotes + optTwoVotes} votes</p>
               {
-                vote === 'second'? (<div className='user-choice' style={{'width': '30px', 'height':'30px', 'fill':'#017a9b'}}><svg viewBox="0 0 33 32"><path d="M17 7a1 1 0 01.117 1.993L17 9h-7v14h14v-7l.007-.117a1 1 0 01.876-.876L25 15a1 1 0 01.993.883L26 16v7a2 2 0 01-2 2H10a2 2 0 01-2-2V9a2 2 0 012-2h7zm8.555.168a1 1 0 01.277 1.387l-8 10a1 1 0 01-1.54.152l-3-3a1 1 0 011.415-1.414l2.138 2.137 7.323-8.985a1 1 0 011.387-.277z"></path></svg></div>):(<p></p>)
+                vote === 'second'? (<div className='user-choice'><svg viewBox="0 0 33 32"><path d="M17 7a1 1 0 01.117 1.993L17 9h-7v14h14v-7l.007-.117a1 1 0 01.876-.876L25 15a1 1 0 01.993.883L26 16v7a2 2 0 01-2 2H10a2 2 0 01-2-2V9a2 2 0 012-2h7zm8.555.168a1 1 0 01.277 1.387l-8 10a1 1 0 01-1.54.152l-3-3a1 1 0 011.415-1.414l2.138 2.137 7.323-8.985a1 1 0 011.387-.277z"></path></svg></div>):(<p></p>)
               }
             </div>
           </div>
